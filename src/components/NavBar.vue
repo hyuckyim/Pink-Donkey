82d57875c55aa1f492160ee56d46d5d62c9074df
Sid <siddharththakur76@gmail.com>
2019-11-26 21:47:03 +1300

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,7 @@
       color="white"
       disable-resize-watcher
     >
-      <v-list dense>
+      <v-list>
         <v-list-item v-for="button in buttons" :key="button.title" :to="button.path">
           <v-list-item-icon>
             <v-icon large color="black">{{ button.icon }}</v-icon>
@@ -18,7 +18,6 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-
     </v-navigation-drawer>
 
     <v-app-bar app color="white">
@@ -26,18 +25,23 @@
         <img src="../assets/logo.jpeg" />
       </v-avatar>
       <v-spacer class="hidden-md-and-up"></v-spacer>
-      
-      <v-app-bar-nav-icon  color="black" x-large class="hidden-md-and-up" @click="drawer = true" />
+
+      <v-app-bar-nav-icon color="black" x-large class="hidden-md-and-up" @click="drawer = true" />
       <template v-slot:extension>
         <v-avatar size="150" class="hidden-sm-and-down">
           <img src="../assets/logo.jpeg" />
         </v-avatar>
       </template>
-      <v-spacer class="hidden-sm-and-down"/>
-
-      <v-btn class="hidden-sm-and-down" v-for="button in buttons" :key="button.title">
-          {{button.title}}
-      </v-btn>
+      <v-spacer class="hidden-sm-and-down" />
+      <v-btn
+        text
+        class="hidden-sm-and-down mt-8 ma-2 title"
+        v-for="button in buttons"
+        :key="button.title"
+      >
+      <v-icon>{{button.icon}}</v-icon>
+      {{button.title}}</v-btn>
+      
     </v-app-bar>
   </span>
 </template>
@@ -48,7 +52,12 @@ export default {
   data() {
     return {
       drawer: false,
-      buttons: [{ title: "About us", icon: "face", path: "/Page1" }]
+      buttons: [
+        { title: "About Us", icon: "face", path: "/Page1" },
+        { title: "Food Trucks", icon: "local_shipping", path: "/FoodTrucks" },
+        { title: "Catering", icon: "outdoor_grill", path: "/catering" },
+        { title: "Contact Us", icon: "local_phone", path: "/contact us" }
+      ]
     };
   }
 };
